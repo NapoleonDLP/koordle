5bffb0780602ada05db73cbca2f3b945ea879387
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 10:53:05 -0800

    Create and import style files for components

--- client/src/Components/Board.jsx
+++ client/src/Components/Board.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Board.css';
 
 const Board = ({ board }) => {
   return (
--- client/src/Components/Game.jsx
+++ client/src/Components/Game.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from './Board';
 import Keyboard from './Keyboard';
+import './Game.css';
 
 const Game = () => {
   const [board, setBoard] = useState([[null, null, null, null, null],
