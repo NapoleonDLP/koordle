5cc074c67085ee72fee00ad6ed02b45596b13ead
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 13:17:30 -0800

    Pass attemptCount hooks to keyboard

--- client/src/Components/Game.jsx
+++ client/src/Components/Game.jsx
@@ -4,13 +4,15 @@ import Keyboard from './Keyboard';
 import './Game.css';
 
 const Game = () => {
-  const [currentWord, setCurrentWord] = useState(null);
-  const [attempt, setAttempt] = useState(0);
+  const [currentWord, setCurrentWord] = useState([]);
+  const [attemptCount, setAttemptCount] = useState(0);
+
+  console.log("CurrentWord from Game component: ", currentWord);
 
   return (
     <>
-      <Board currentWord={ currentWord } attempt={ attempt }></Board>
-      <Keyboard currentWord={ currentWord } attempt={ attempt }></Keyboard>
+      <Board currentWord={ currentWord } attemptCount={ attemptCount }></Board>
+      <Keyboard currentWord={ currentWord } setCurrentWord={ setCurrentWord } attemptCount={ attemptCount } setAttemptCount={ setAttemptCount }></Keyboard>
     </>
   )
 };
