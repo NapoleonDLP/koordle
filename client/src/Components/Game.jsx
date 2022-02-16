7e433b6a768634e159525d5a938f6a2bb8634288
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 12:03:27 -0800

    Move states to their specific components add two new states for game

--- client/src/Components/Game.jsx
+++ client/src/Components/Game.jsx
@@ -4,22 +4,13 @@ import Keyboard from './Keyboard';
 import './Game.css';
 
 const Game = () => {
-  const [board, setBoard] = useState([[null, null, null, null, null],
-                                      [null, null, null, null, null],
-                                      [null, null, null, null, null],
-                                      [null, 'A', null, null, null],
-                                      [null, null, null, null, null],
-                                      [null, null, null, null, null]
-                                      ]);
+  const [currentWord, setCurrentWord] = useState(null);
+  const [attempt, setAttempt] = useState(0);
 
-  const [ keyboard, setKeyboard ] = useState([['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-                                              ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-                                              ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Delete']
-                                            ]);
   return (
     <>
-      <Board board={ board }></Board>
-      <Keyboard keyboard={ keyboard }></Keyboard>
+      <Board currentWord={ currentWord } attempt={ attempt }></Board>
+      <Keyboard currentWord={ currentWord } attempt={ attempt }></Keyboard>
     </>
   )
 };
