ac953d5849b6ad592cb945f56d114c4feae37bbb
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 11:24:31 -0800

    Build out board

--- client/src/Components/Board.jsx
+++ client/src/Components/Board.jsx
@@ -5,10 +5,15 @@ const Board = ({ board }) => {
   return (
     <div className='board'>
       {board.map((row, rowIndex) => (
-        <ul id={ 'row-' + rowIndex } className='row'>
+        <ul id={ 'row-' + rowIndex } className='row' key={rowIndex}>
           {row.map((square, squareIndex) => {
+            console.log(square)
             let location = rowIndex + '-' + squareIndex;
-            return <li id={'square-' + location } className='square' key={[rowIndex, squareIndex]}></li>
+            return (
+              <li id={'square-' + location } className='square' key={[rowIndex, squareIndex]}>
+                <h2>{ square }</h2>
+              </li>
+              )
           })}
         </ul>
       ))}
