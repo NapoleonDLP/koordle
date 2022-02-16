94dd879de8cccb8d50412f3a11d75b0495792db2
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 13:18:18 -0800

    Assign key props to elements

--- client/src/Components/Keyboard.jsx
+++ client/src/Components/Keyboard.jsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react';
 import './Keyboard.css';
 
-const Keyboard = ({ currentWord }) => {
-    const [ keyboard, setKeyboard ] = useState([['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+const Keyboard = ({ setCurrentWord, currentWord }) => {
+
+
+  const [ keyboard, setKeyboard ] = useState([['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
                                               ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
                                               ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Delete']
                                             ]);
 
+  const handleKeyboardClick = (e) => {
+    if (currentWord.length < 5 && e.target.innerText !== 'Enter' && e.target.innerText !== 'Delete') {
+      setCurrentWord(currentWord.concat([e.target.innerText]));
+    } else if (e.target.innerText === 'Delete') {
+      let lastRemoved = [...currentWord];
+      lastRemoved.pop();
+      setCurrentWord(lastRemoved);
+    } else if (currentWord.length === 5 && e.target.innerText === 'Enter') {
+      console.log('keyboard enter')
+        //RUN THE CHECK ON THE ROW TO SEE WHAT IS CORRECT and CHANGE CSS to SHOW THAT
+        //Update keyboard to reflect that
+    }
+  }
+
   return (
     <div className='board'>
       {
-        keyboard.map((row) => (
-          <ul className="row">
+        keyboard.map((row, keyBoardRowIndex) => (
+          <ul className="row" key={ keyBoardRowIndex }>
             {
-              row.map((keyboardButton) => (
-                <li className='square'>
-                  <h3>{ keyboardButton }</h3>
+              row.map((keyboardButton, keyboardButtonIndex) => (
+                <li onClick={ handleKeyboardClick } className='square' key={ keyboardButtonIndex }>
+                  { keyboardButton }
                 </li>
               ))
             }
