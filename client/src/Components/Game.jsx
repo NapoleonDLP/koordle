6aa10dbc67072b0794906e66b9a3b12a0a3d983c
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-16 21:33:39 -0800

    Keep state of letter in correct place and letter in incorrect place

--- client/src/Components/Game.jsx
+++ client/src/Components/Game.jsx
@@ -6,11 +6,27 @@ import './Game.css';
 const Game = () => {
   const [currentWord, setCurrentWord] = useState([]);
   const [attemptCount, setAttemptCount] = useState(0);
+  const [correctLetterWrongPlace, setCorrectLetterWrongPlace] = useState([]);
+  const [correctLetterRightPlace, setCorrectLetterRightPlace] = useState([]);
 
   return (
     <>
-      <Board currentWord={ currentWord } attemptCount={ attemptCount }></Board>
-      <Keyboard currentWord={ currentWord } setCurrentWord={ setCurrentWord } attemptCount={ attemptCount } setAttemptCount={ setAttemptCount }></Keyboard>
+      <Board
+        currentWord={ currentWord }
+        attemptCount={ attemptCount }
+        correctLetterWrongPlace={ correctLetterWrongPlace }
+        correctLetterRightPlace={ correctLetterRightPlace }
+        ></Board>
+      <Keyboard
+        currentWord={ currentWord }
+        setCurrentWord={ setCurrentWord }
+        attemptCount={ attemptCount }
+        setAttemptCount={ setAttemptCount }
+        correctLetterRightPlace={ correctLetterRightPlace }
+        setCorrectLetterRightPlace={ setCorrectLetterRightPlace }
+        correctLetterWrongPlace={ correctLetterWrongPlace }
+        setCorrectLetterWrongPlace={ setCorrectLetterWrongPlace }
+        ></Keyboard>
     </>
   )
 };
