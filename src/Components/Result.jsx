d7c9851208f6367f58885d1f73fa8e33329a17f9
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-24 15:39:47 -0800

    Add play again button

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Result = ({ result }) => {
   console.log('RESULT: ', result)
   return (
-      (result === null ? null : (result ? <h1>WINNNAH!</h1> : <h1>YOU LOOSE!</h1>))
+    (result === null ? null : (
+      <>
+        { result ? <h1>WINNNAH!</h1> : <h1>YOU LOOSE!</h1> }
+        <button onClick={() => window.location.reload(false)}> Play Again </button>
+      </>
+    ))
   )
 }
 
