12f06bc81311353d47d1b4796b8c22804fbe02c3
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-24 20:49:02 -0800

    Change url variable

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -16,7 +16,7 @@ const Game = () => {
   }, [])
 
   const loadWord = () => {
-    fetch(process.env.REACT_APP_KOORDLE_API_LOCAL + '/new-word')
+    fetch(process.env.REACT_APP_KOORDLE_API_URL + '/new-word')
     .then(data => data.json())
     .then(word => setAnswer(word.newWord))
     .catch(e => console.log(e))
