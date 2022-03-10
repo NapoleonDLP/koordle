819aeafc63ca974061a584c4b4967d5f36badaba
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-09 22:55:28 -0800

    Build login function

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [ userEmail, setUserEmail ] = useState(null);
+  const [ password, setPassword ] = useState(null);
 
+  const handleChange = (event) => {
+
+  }
+
+  const handleSubmit = (event) => {
+
+  }
+
+  return (
+    <div>
+      <form>
+        <label>
+          Email:
+          <input type='text' value={ userEmail } onChange={ handleChange }></input>
+        </label>
+        <label>
+          Password:
+          <input type='password' value={ password } onChange={ handleChange }></input>
+        </label>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
