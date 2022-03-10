cd2670374f566182befd48cb249a955cc69ce8f7
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-09 23:20:48 -0800

    Add Routes for home and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+
 import Game from './Components/Game';
+import Login from './Components/Login';
+
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Game></Game>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Game /> } />
+          <Route path='/login' element={ <Login /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
